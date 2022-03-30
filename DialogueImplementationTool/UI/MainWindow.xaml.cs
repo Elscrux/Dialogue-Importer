37c6246fd6c663b0bc062be04861ed74330a0732
nickp <nickposer2102@gmail.com>
2022-03-30 23:19:56 +0200

    Added message when plugins.txt doesn't exist

--- DialogueImplementationTool/UI/MainWindow.xaml.cs
+++ DialogueImplementationTool/UI/MainWindow.xaml.cs
@@ -1,10 +1,17 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using DialogueImplementationTool.Parser;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Environments.DI;
+using Mutagen.Bethesda.Plugins.Order.DI;
 
 namespace DialogueImplementationTool.UI;
 
 public partial class MainWindow {
     public MainWindow() {
+        var pathProvider = new PluginListingsPathProvider(new GameReleaseInjection(GameRelease.SkyrimSE));
+        if (!File.Exists(pathProvider.Path)) MessageBox.Show($"Make sure {pathProvider.Path} exists.");
+
         InitializeComponent();
         DataContext = App.DialogueVM;
     }
