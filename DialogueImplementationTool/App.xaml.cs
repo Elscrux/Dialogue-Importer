faec97bdef10a93b4e4cde79ee39b43a264f850f
nickp <nickposer2102@gmail.com>
2022-03-31 00:22:04 +0200

    Added crash logs

--- DialogueImplementationTool/App.xaml.cs
+++ DialogueImplementationTool/App.xaml.cs
@@ -5,13 +5,7 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Media;
 using DialogueImplementationTool.UI;
-using Microsoft.Win32;
-using Syncfusion.SfSkinManager;
-using Syncfusion.Themes.MaterialDark.WPF;
-using Syncfusion.Themes.MaterialLight.WPF;
 
 namespace DialogueImplementationTool; 
 
@@ -21,41 +15,14 @@ namespace DialogueImplementationTool;
 public partial class App {
     public static readonly DialogueVM DialogueVM = new();
     
-    private const string RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
-    private const string RegistryValueName = "AppsUseLightTheme";
-    
-    private enum WindowsTheme {
-        Light,
-        Dark
-    }
-
-    private static WindowsTheme GetTheme() {
-        using var key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath);
-        var registryValueObject = key?.GetValue(RegistryValueName);
-        if (registryValueObject == null) return WindowsTheme.Light;
-
-        var registryValue = (int) registryValueObject;
-        return registryValue == 1 ? WindowsTheme.Light : WindowsTheme.Dark;
+    public App() {
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnFirstChanceException;
     }
     
-    public static void UpdateTheme(DependencyObject dependencyObject) {
-        SfSkinManager.ApplyStylesOnApplication = true;
-        switch (GetTheme()) {
-            case WindowsTheme.Dark:
-                var darkThemeSettings = new MaterialDarkThemeSettings {
-                    PrimaryBackground = new SolidColorBrush(SystemParameters.WindowGlassColor)
-                };
-                SfSkinManager.RegisterThemeSettings("MaterialDark", darkThemeSettings);
-                SfSkinManager.SetTheme(dependencyObject, new Theme("MaterialDark"));
-
-                break;
-            case WindowsTheme.Light:
-                var lightThemeSettings = new MaterialLightThemeSettings {
-                    PrimaryBackground = new SolidColorBrush(SystemParameters.WindowGlassColor)
-                };
-                SfSkinManager.RegisterThemeSettings("MaterialLight", lightThemeSettings);
-                SfSkinManager.SetTheme(dependencyObject, new Theme("MaterialLight"));
-                break;
-        }
+    private void CurrentDomainOnFirstChanceException(object sender, UnhandledExceptionEventArgs e) {
+        var exception = (Exception) e.ExceptionObject;
+        
+        using var log = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CrashLog.txt"), false);
+        log.WriteLine(exception);
     }
 }
