2a19fa94665e2934bea9456f8f28a4d83fa8db47
nickp <nickposer2102@gmail.com>
2022-11-05 15:08:41 +0100

    Improved script note color tolerance

--- DialogueImplementationTool/Dialogue/Responses/ScriptNotesParser.cs
+++ DialogueImplementationTool/Dialogue/Responses/ScriptNotesParser.cs
@@ -7,11 +7,11 @@ namespace DialogueImplementationTool.Dialogue.Responses;
 public class ScriptNotesParser : IDialogueResponsePreProcessor {
     private static readonly Regex InvalidBracesRegex = new(@"\[([^\[\]]*)\]");
 
-    private static readonly Color OrangeColor = Color.Orange; 
-    private const int SimilarThreshold = 20; 
-    
+    private static readonly Color OrangeColor = Color.Orange;
+    private static readonly Color SimilarThreshold = Color.FromArgb(64, 64, 16);
+
     public DialogueResponse Process(DialogueResponse response, FormattedText text) {
-        if (AreColorsSimilar(text.Color, OrangeColor)) {
+        if (AreColorsSimilar(text.Color, OrangeColor, SimilarThreshold)) {
             var match = InvalidBracesRegex.Match(text.Text);
             return new DialogueResponse { ScriptNote = match.Success ? match.Groups[1].Value : text.Text };
         }
@@ -19,9 +19,9 @@ public class ScriptNotesParser : IDialogueResponsePreProcessor {
         return response;
     }
 
-    private bool AreColorsSimilar(Color color1, Color color2) {
-        return Math.Abs(color1.R - color2.R) < SimilarThreshold
-         && Math.Abs(color1.G - color2.G) < SimilarThreshold
-         && Math.Abs(color1.B - color2.B) < SimilarThreshold;
+    private bool AreColorsSimilar(Color color1, Color color2, Color thresholdColor) {
+        return Math.Abs(color1.R - color2.R) < thresholdColor.R
+         && Math.Abs(color1.G - color2.G) < thresholdColor.G
+         && Math.Abs(color1.B - color2.B) < thresholdColor.B;
     }
 }
