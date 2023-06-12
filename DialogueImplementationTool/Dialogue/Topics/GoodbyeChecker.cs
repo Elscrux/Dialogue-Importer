0a634f7486387fc8f03adff96dfd54bc4a5c5339
nickp <nickposer2102@gmail.com>
2023-06-12 22:41:41 +0200

    Use regex for goodbye checker

--- DialogueImplementationTool/Dialogue/Topics/GoodbyeChecker.cs
+++ DialogueImplementationTool/Dialogue/Topics/GoodbyeChecker.cs
@@ -1,15 +1,16 @@
-﻿using System;
-namespace DialogueImplementationTool.Dialogue.Topics; 
+﻿using System.Text.RegularExpressions;
+namespace DialogueImplementationTool.Dialogue.Topics;
 
 public class GoodbyeChecker : IDialogueTopicPostProcessor {
+	private static readonly Regex GoodbyeRegex = new(@"\[(exit|end) (dialog|dialogue|conversation|convo)\]");
+
 	public void Process(DialogueTopic topic) {
 		if (topic.Responses.Count == 0) return;
 
 		var response = topic.Responses[0];
 		var previousResponse = response.Response;
 
-		response.Response = response.Response
-			.Replace("[exit conversation]", string.Empty, StringComparison.OrdinalIgnoreCase);
+		response.Response = GoodbyeRegex.Replace(response.Response, string.Empty);
 
 		if (response.Response != previousResponse) {
 			topic.SayOnce = true;
