9423132c190ecd7d94469827285d811275d83950
nickp <nickposer2102@gmail.com>
2023-07-26 11:09:26 +0200

    Add more alternative versions of back to dialogue

--- DialogueImplementationTool/Dialogue/Responses/BackToDialogueRemover.cs
+++ DialogueImplementationTool/Dialogue/Responses/BackToDialogueRemover.cs
@@ -4,7 +4,10 @@ namespace DialogueImplementationTool.Dialogue.Responses;
 public class BackToDialogueRemover : IDialogueResponsePostProcessor {
     public void Process(DialogueResponse response) {
         response.Response = response.Response
-            .Replace("[back to root]", string.Empty, StringComparison.InvariantCulture)
-            .Replace("[back to dialogue]", string.Empty, StringComparison.InvariantCulture);
+            .Replace("[back to root]", string.Empty, StringComparison.OrdinalIgnoreCase)
+            .Replace("[back to top dialogue]", string.Empty, StringComparison.OrdinalIgnoreCase)
+            .Replace("[back to top level dialogue]", string.Empty, StringComparison.OrdinalIgnoreCase)
+            .Replace("[back to dialogue options]", string.Empty, StringComparison.OrdinalIgnoreCase)
+            .Replace("[back to dialogue]", string.Empty, StringComparison.OrdinalIgnoreCase);
     }
 }
