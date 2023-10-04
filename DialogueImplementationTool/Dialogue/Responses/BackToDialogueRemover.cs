9f22f0fefb670768889b33271912d96365099a30
nickp <nickposer2102@gmail.com>
2023-10-04 21:05:15 +0200

    Improve regex

--- DialogueImplementationTool/Dialogue/Responses/BackToDialogueRemover.cs
+++ DialogueImplementationTool/Dialogue/Responses/BackToDialogueRemover.cs
@@ -2,7 +2,7 @@
 namespace DialogueImplementationTool.Dialogue.Responses; 
 
 public class BackToDialogueRemover : IDialogueResponsePostProcessor {
-	private readonly Regex _regex = new(@"(?i)\[back to (root|top|main)( level( (dialogue|options))?)?\]");
+	private readonly Regex _regex = new(@"(?i)\[back to (root|top|main)( level)?( dialogue)?( options)?\]");
     public void Process(DialogueResponse response) {
 	    response.Response = _regex.Replace(response.Response, string.Empty);
     }
