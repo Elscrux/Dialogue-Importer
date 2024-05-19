1ab48a308c7af852cb7d18af66284a62d6bf8aa7
Elscrux <nickposer2102@gmail.com>
2024-05-19 21:09:42 +0200

    Separate into functions

--- DialogueImplementationTool/Dialogue/Processor/KeywordLinker.cs
+++ DialogueImplementationTool/Dialogue/Processor/KeywordLinker.cs
@@ -26,6 +26,40 @@ public sealed partial class KeywordLinker : IConversationProcessor {
     private static partial Regex OptionsLinkRegex();
 
     public void Process(IList<GeneratedDialogue> dialogues) {
+        ProcessKeywordLinks(dialogues);
+        ProcessOptionLinks(dialogues);
+    }
+
+    private static void ProcessOptionLinks(IList<GeneratedDialogue> dialogues) {
+        var optionsDestinations =
+            GetKeywordTopicInfoDictionary(dialogues, info => OptionsDestinationRegex().Match(info.Responses[^1].Response));
+        var optionsLinks =
+            GetAllKeywordTopicInfo(dialogues, info => OptionsLinkRegex().Match(info.Responses[^1].Response));
+
+        foreach (var (keyword, _, linkTopicInfo) in optionsLinks) {
+            if (linkTopicInfo.Links.Count > 0) {
+                Console.WriteLine($"Keyword {keyword} already has links in dialogue {linkTopicInfo.Prompt}");
+                continue;
+            }
+
+            if (!optionsDestinations.TryGetValue(keyword, out var destination)) {
+                Console.WriteLine($"Keyword {keyword} does not have a destination in any dialogue");
+                continue;
+            }
+
+            destination.TopicInfo.Responses[^1].Response = OptionsDestinationRegex()
+                .Replace(destination.TopicInfo.Responses[^1].Response, string.Empty)
+                .Trim();
+
+            linkTopicInfo.Responses[^1].Response = OptionsLinkRegex()
+                .Replace(linkTopicInfo.Responses[^1].Response, string.Empty)
+                .Trim();
+
+            linkTopicInfo.Links.AddRange(destination.TopicInfo.Links);
+        }
+    }
+
+    private static void ProcessKeywordLinks(IList<GeneratedDialogue> dialogues) {
         var keywordDestination =
             GetKeywordTopicInfoDictionary(dialogues, info => KeywordDestinationRegex().Match(info.Responses[0].Response));
         var keywordLinks =
@@ -52,33 +86,6 @@ public sealed partial class KeywordLinker : IConversationProcessor {
 
             linkTopicInfo.Links.Add(destination.Topic);
             linkTopicInfo.InvisibleContinue = true;
-        }
-
-        var optionsDestinations =
-            GetKeywordTopicInfoDictionary(dialogues, info => OptionsDestinationRegex().Match(info.Responses[^1].Response));
-        var optionsLinks =
-            GetAllKeywordTopicInfo(dialogues, info => OptionsLinkRegex().Match(info.Responses[^1].Response));
-
-        foreach (var (keyword, _, linkTopicInfo) in optionsLinks) {
-            if (linkTopicInfo.Links.Count > 0) {
-                Console.WriteLine($"Keyword {keyword} already has links in dialogue {linkTopicInfo.Prompt}");
-                continue;
-            }
-
-            if (!optionsDestinations.TryGetValue(keyword, out var destination)) {
-                Console.WriteLine($"Keyword {keyword} does not have a destination in any dialogue");
-                continue;
-            }
-
-            destination.TopicInfo.Responses[^1].Response = OptionsDestinationRegex()
-                .Replace(destination.TopicInfo.Responses[^1].Response, string.Empty)
-                .Trim();
-
-            linkTopicInfo.Responses[^1].Response = OptionsLinkRegex()
-                .Replace(linkTopicInfo.Responses[^1].Response, string.Empty)
-                .Trim();
-
-            linkTopicInfo.Links.AddRange(destination.TopicInfo.Links);
         }
     }
 
