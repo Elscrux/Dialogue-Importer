3aeb1a2777df069e134a632fea29d3bbea223db9
Elscrux <nickposer2102@gmail.com>
2024-05-22 14:59:12 +0200

    Add CollapseNoteOnlyResponse processor

---
+++ DialogueImplementationTool.Tests/Processor/TestCollapseNoteOnlyResponse.cs
@@ -0,0 +1,28 @@
+﻿using DialogueImplementationTool.Dialogue.Model;
+using DialogueImplementationTool.Dialogue.Processor;
+using FluentAssertions;
+namespace DialogueImplementationTool.Tests.Processor;
+
+public sealed class TestCollapseNoteOnlyResponse {
+    [Fact]
+    public void TestProcess() {
+        // Arrange
+        var topicInfo = new DialogueTopicInfo {
+            Prompt = "Hi, Player. How are you?",
+            Responses = [
+                new DialogueResponse { Response = "I'm good." },
+                new DialogueResponse { StartNotes = [new Note { Text = "[back to options]" }] },
+            ]
+        };
+
+        // Act
+        var collapseNoteOnlyResponse = new CollapseNoteOnlyResponse();
+        collapseNoteOnlyResponse.Process(topicInfo);
+
+        // Assert
+        topicInfo.Prompt.Should().Be("Hi, Player. How are you?");
+        topicInfo.Responses.Should().HaveCount(1);
+        topicInfo.Responses[0].Response.Should().Be("I'm good.");
+        topicInfo.Responses[0].HasNote("[back to options]").Should().BeTrue();
+    }
+}
---
+++ DialogueImplementationTool/Dialogue/Processor/CollapseNoteOnlyResponse.cs
@@ -0,0 +1,36 @@
+﻿using DialogueImplementationTool.Dialogue.Model;
+using Noggog;
+namespace DialogueImplementationTool.Dialogue.Processor;
+
+/// <summary>
+/// Merges responses that have no text and just notes into the previous response.
+/// <example>
+/// <para>Here we merge the [back to options] note in 1.2 into 1.1.</para>
+/// <para>Before:</para>
+/// <code>
+/// <para>1. Hi, Player. How are you?</para>
+/// <para>	1.1. I'm good.</para>
+/// <para>	1.2. [back to options]</para>
+/// </code>
+/// <para>After:</para>
+/// <code>
+/// <para>1. Hi, Player. How are you?</para>
+/// <para>	1.1. I'm good. [back to options]</para>
+/// </code>
+/// </example>
+/// </summary>
+public sealed class CollapseNoteOnlyResponse : IDialogueTopicInfoProcessor {
+    public void Process(DialogueTopicInfo topicInfo) {
+        var counter = 1;
+        while (counter < topicInfo.Responses.Count) {
+            var response = topicInfo.Responses[counter];
+
+            if (response.Response.IsNullOrEmpty() && response.Notes().Count > 0) {
+                topicInfo.Responses[counter - 1].EndsNotes.AddRange(response.Notes());
+                topicInfo.Responses.RemoveAt(counter);
+            } else {
+                counter++;
+            }
+        }
+    }
+}
