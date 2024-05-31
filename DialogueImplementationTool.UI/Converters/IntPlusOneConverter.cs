c11f57e91a9353453ed372115915cb5a18ec49c6
Elscrux <nickposer2102@gmail.com>
2024-05-31 18:14:01 +0200

    Don't use zero based indices in display

---
+++ DialogueImplementationTool.UI/Converters/IntPlusOneConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+namespace DialogueImplementationTool.UI.Converters;
+
+public class IntPlusOneConverter : IValueConverter {
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is int i) return i + 1;
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is int i) return i - 1;
+
+        return null;
+    }
+}
