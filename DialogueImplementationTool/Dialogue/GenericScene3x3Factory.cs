038b83766b4a9f7876a879d8b6f1e4374c69cdb9
Elscrux <nickposer2102@gmail.com>
2024-05-31 15:24:33 +0200

    Remove colons at the end of 3x3 scene speaker names

--- DialogueImplementationTool/Dialogue/GenericScene3x3Factory.cs
+++ DialogueImplementationTool/Dialogue/GenericScene3x3Factory.cs
@@ -5,6 +5,8 @@ namespace DialogueImplementationTool.Dialogue;
 
 public sealed class GenericScene3x3Factory(IDialogueContext context) : GenericGenericSceneFactory(context) {
     public override IDialogueProcessor ConfigureProcessor(DialogueProcessor dialogueProcessor) {
+        // Remove trailing colons from prompts (speaker names)
+        dialogueProcessor.TopicInfoProcessors.Add(new CustomTopicInfoTrimmer(":"));
         // Skip pre-processing of regular scene factory
         return dialogueProcessor;
     }
---
+++ DialogueImplementationTool/Dialogue/Processor/CustomTopicInfoTrimmer.cs
@@ -0,0 +1,11 @@
+﻿using DialogueImplementationTool.Dialogue.Model;
+using Noggog;
+namespace DialogueImplementationTool.Dialogue.Processor;
+
+public sealed class CustomTopicInfoTrimmer(string trim) : IDialogueTopicInfoProcessor {
+    public void Process(DialogueTopicInfo topicInfo) {
+        topicInfo.Prompt.Text = topicInfo.Prompt.Text.TrimEnd(trim).TrimStart(trim).Trim();
+        topicInfo.Responses.ForEach(r => r.Response = r.Response.TrimEnd(trim).TrimStart(trim).Trim());
+        topicInfo.Responses.RemoveWhere(r => r.IsEmpty() && r.Notes().Count == 0);
+    }
+}
