8571853a5e8806c420712e95bbba31208b0c51db
Elscrux <nickposer2102@gmail.com>
2024-06-15 23:46:37 +0200

    Only add condition once for Offers services

--- DialogueImplementationTool/Dialogue/Processor/OffersServicesChecker.cs
+++ DialogueImplementationTool/Dialogue/Processor/OffersServicesChecker.cs
@@ -1,24 +1,28 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 using DialogueImplementationTool.Dialogue.Model;
 using Mutagen.Bethesda.Skyrim;
 namespace DialogueImplementationTool.Dialogue.Processor;
 
 public sealed partial class OffersServicesChecker : IDialogueTopicInfoProcessor {
-    [GeneratedRegex("(?:if|when|is) (?:at|in|inside) (?:.* )?(?:store|stall)")]
+    [GeneratedRegex("(?:if|when|is) (?:at|in|inside) (?:.* )?(?:store|stall)", RegexOptions.IgnoreCase)]
     private static partial Regex OffersServicesRegex();
 
     public void Process(DialogueTopicInfo topicInfo) {
         foreach (var response in topicInfo.Responses) {
-            foreach (var note in response.Notes()) {
-                var match = OffersServicesRegex().Match(note.Text);
-                if (!match.Success) continue;
+            var notes = response.Notes()
+                .Where(x => OffersServicesRegex().IsMatch(x.Text))
+                .ToList();
 
-                topicInfo.ExtraConditions.Add(new ConditionFloat {
-                    Data = new GetOffersServicesNowConditionData(),
-                    CompareOperator = CompareOperator.EqualTo,
-                    ComparisonValue = 1,
-                });
+            if (notes.Count == 0) continue;
 
+            topicInfo.ExtraConditions.Add(new ConditionFloat {
+                Data = new GetOffersServicesNowConditionData(),
+                CompareOperator = CompareOperator.EqualTo,
+                ComparisonValue = 1,
+            });
+
+            foreach (var note in notes) {
                 response.RemoveNote(note);
             }
         }
