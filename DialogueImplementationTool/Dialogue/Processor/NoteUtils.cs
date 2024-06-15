fc0097e488a8dd7af78cc19cf3df0b18b9efb4d4
Elscrux <nickposer2102@gmail.com>
2024-06-15 16:36:02 +0200

    Allow multiple [ or ] in a note definition

--- DialogueImplementationTool/Dialogue/Processor/NoteUtils.cs
+++ DialogueImplementationTool/Dialogue/Processor/NoteUtils.cs
@@ -2,12 +2,11 @@
 namespace DialogueImplementationTool.Dialogue.Processor;
 
 public static partial class NoteUtils {
-    private const string NotePattern = @"\[([^\]]*)\]";
+    private const string NotePattern = @"\[+([^\]]*)\]+";
 
     [GeneratedRegex(@$"^\s*{NotePattern}")]
     public static partial Regex StartNoteRegex();
 
     [GeneratedRegex(@$"{NotePattern}\s*$")]
     public static partial Regex EndNoteRegex();
-
 }
