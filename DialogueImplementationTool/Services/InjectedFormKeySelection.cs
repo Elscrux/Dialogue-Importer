5a254801f10b0eb926e99534127923f18e52b336
Elscrux <nickposer2102@gmail.com>
2024-08-05 17:38:27 +0200

    feat: Add injectable dictionary in FormKeySelection

--- DialogueImplementationTool/Services/InjectedFormKeySelection.cs
+++ DialogueImplementationTool/Services/InjectedFormKeySelection.cs
@@ -1,8 +1,16 @@
-﻿using Mutagen.Bethesda.Plugins;
+﻿using System.Collections.Generic;
+using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Plugins.Records;
 namespace DialogueImplementationTool.Services;
 
 public class InjectedFormKeySelection : IFormKeySelection {
+    private readonly Dictionary<string, FormKey> _injectedEntries = new();
+
+    public InjectedFormKeySelection() {}
+    public InjectedFormKeySelection(Dictionary<string, FormKey> injectedEntries) {
+        _injectedEntries = injectedEntries;
+    }
+
     public FormKey GetFormKey<TMajor>(string title, FormKey defaultFormKey) 
-        where TMajor : IMajorRecordQueryableGetter => defaultFormKey;
+        where TMajor : IMajorRecordQueryableGetter => _injectedEntries.GetValueOrDefault(title, defaultFormKey);
 }
