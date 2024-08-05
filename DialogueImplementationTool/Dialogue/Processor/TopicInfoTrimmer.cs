29a4006056c444e3f48a44540f3112929e9f09db
Elscrux <nickposer2102@gmail.com>
2024-08-05 17:37:38 +0200

    fix: Only trim non-shared info responses

--- DialogueImplementationTool/Dialogue/Processor/TopicInfoTrimmer.cs
+++ DialogueImplementationTool/Dialogue/Processor/TopicInfoTrimmer.cs
@@ -5,7 +5,10 @@ namespace DialogueImplementationTool.Dialogue.Processor;
 public sealed class TopicInfoTrimmer : IDialogueTopicInfoProcessor {
     public void Process(DialogueTopicInfo topicInfo) {
         topicInfo.Prompt.Text = topicInfo.Prompt.Text.Trim();
-        topicInfo.Responses.ForEach(r => r.Response = r.Response.Trim());
-        topicInfo.Responses.RemoveWhere(r => r.IsEmpty() && r.Notes().Count == 0);
+
+        if (topicInfo.SharedInfo is null) {
+            topicInfo.Responses.ForEach(r => r.Response = r.Response.Trim());
+            topicInfo.Responses.RemoveWhere(r => r.IsEmpty() && r.Notes().Count == 0);
+        }
     }
 }
