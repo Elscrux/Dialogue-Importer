b44c5e35351d093eab1c4539e76e482ba18e93b6
Elscrux <nickposer2102@gmail.com>
2024-12-10 23:51:05 +0100

    refactor: Replace RGB based color similarity approach with a hue based approach

--- DialogueImplementationTool/Dialogue/Processor/ScriptNotesParser.cs
+++ DialogueImplementationTool/Dialogue/Processor/ScriptNotesParser.cs
@@ -8,11 +8,11 @@ namespace DialogueImplementationTool.Dialogue.Processor;
 
 public sealed class ScriptNotesParser : IDialogueResponseProcessor {
     private static readonly Color OrangeColor = Color.Orange;
-    private static readonly Color SimilarThreshold = Color.FromArgb(64, 64, 16);
 
     public void Process(DialogueResponse response, IReadOnlyList<FormattedText> textSnippets) {
+
         var scriptNotes = response.Notes()
-            .Where(note => note.Colors.Any(color => AreColorsSimilar(color, OrangeColor, SimilarThreshold)))
+            .Where(note => note.Colors.Any(color => AreColorsSimilar(color, OrangeColor, 20)))
             .ToList();
 
         response.ScriptNote = string.Join(' ', scriptNotes.Select(x => x.Text));
@@ -22,9 +22,10 @@ public sealed class ScriptNotesParser : IDialogueResponseProcessor {
         }
     }
 
-    private static bool AreColorsSimilar(Color color1, Color color2, Color thresholdColor) {
-        return Math.Abs(color1.R - color2.R) < thresholdColor.R
-         && Math.Abs(color1.G - color2.G) < thresholdColor.G
-         && Math.Abs(color1.B - color2.B) < thresholdColor.B;
+    private static bool AreColorsSimilar(Color color1, Color color2, float threshold) {
+        var h1 = color1.GetHue();
+        var h2 = color2.GetHue();
+
+        return Math.Abs(h1 - h2) < threshold;
     }
 }
