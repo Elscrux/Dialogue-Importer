5bd4b0b61de00eee4d2b7e10d214ce6766b22f85
Elscrux <nickposer2102@gmail.com>
2025-02-23 13:10:34 +0100

    refactor: Use function to remove duplicate initializers

--- DialogueImplementationTool/Dialogue/Processor/ActiveSpellProcessor.cs
+++ DialogueImplementationTool/Dialogue/Processor/ActiveSpellProcessor.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using DialogueImplementationTool.Dialogue.Model;
 using DialogueImplementationTool.Extension;
 using Mutagen.Bethesda.FormKeys.SkyrimSE;
+using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
 namespace DialogueImplementationTool.Dialogue.Processor;
 
@@ -12,22 +13,20 @@ public sealed class ActiveSpellProcessor : DialogueTypeProcessor {
     protected override IEnumerable<Condition> GetConditions(string description, DialogueTopicInfo topicInfo) {
         switch (description) {
             case "Player is seen with an active flame spell":
-                yield return new SpellHasKeywordConditionData {
-                    RunOnType = Condition.RunOnType.Reference,
-                    Reference = Skyrim.PlayerRef,
-                    Keyword = { Link = { FormKey = Skyrim.Keyword.MagicDamageFire.FormKey } },
-                }.ToConditionFloat();
+                yield return PlayerCastsSpellWithKeyword(Skyrim.Keyword.MagicDamageFire);
 
                 break;
 
             case "Player is seen with an active frost spell":
-                yield return new SpellHasKeywordConditionData {
-                    RunOnType = Condition.RunOnType.Reference,
-                    Reference = Skyrim.PlayerRef,
-                    Keyword = { Link = { FormKey = Skyrim.Keyword.MagicDamageFrost.FormKey } },
-                }.ToConditionFloat();
+                yield return PlayerCastsSpellWithKeyword(Skyrim.Keyword.MagicDamageFrost);
 
                 break;
         }
+
+        Condition PlayerCastsSpellWithKeyword(IFormLinkGetter formLink) => new SpellHasKeywordConditionData {
+            RunOnType = Condition.RunOnType.Reference,
+            Reference = Skyrim.PlayerRef,
+            Keyword = { Link = { FormKey = formLink.FormKey } },
+        }.ToConditionFloat();
     }
 }
